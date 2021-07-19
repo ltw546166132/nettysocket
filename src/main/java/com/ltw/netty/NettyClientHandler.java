00999cfc7aaa876d36f0013693549d9b216891e1
ltw <litianwei546166132@outlook.com>
2021-07-19 14:22:19 +0800

    submit

--- src/main/java/com/ltw/netty/NettyClientHandler.java
+++ src/main/java/com/ltw/netty/NettyClientHandler.java
@@ -6,13 +6,21 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
 
+/**
+ * @author ltw
+ */
 public class NettyClientHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         ctx.writeAndFlush(Unpooled.copiedBuffer("hello, server ", CharsetUtil.UTF_8));
     }
 
-    //当通道有读取事件，会触发
+    /**
+     * 当通道有读取事件，会触发
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf byteBuf = (ByteBuf) msg;
