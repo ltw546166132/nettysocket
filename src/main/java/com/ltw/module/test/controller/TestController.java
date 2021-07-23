fbf92d332ebec13848f815c63c58ec9d91fd6f4a
“litianwei” <litianwei546166132@outlook.com>
2021-07-23 17:47:09 +0800

    submit

---
+++ src/main/java/com/ltw/module/test/controller/TestController.java
@@ -0,0 +1,9 @@
+package com.ltw.module.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+}
