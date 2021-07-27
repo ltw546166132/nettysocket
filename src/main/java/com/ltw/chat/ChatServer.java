b9ab57d9c821930bf57adf73d4e33755259a4591
“litianwei” <litianwei546166132@outlook.com>
2021-07-27 18:12:41 +0800

    submit

---
+++ src/main/java/com/ltw/chat/ChatServer.java
@@ -0,0 +1,42 @@
+package com.ltw.chat;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class ChatServer {
+    public static void main(String[] args) throws InterruptedException {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            //设置两个线程组
+            serverBootstrap.group(bossGroup, workerGroup)
+                    //使用NioSocketChannel 作为服务器通道实现;
+                    .channel(NioServerSocketChannel.class)
+                    //设置线程队列得到的连接个数
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    //设置保持活动链接状态
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            ChannelPipeline pipeline = socketChannel.pipeline();
+                            pipeline.addLast("decoder", new StringDecoder());
+                            pipeline.addLast("encoder", new StringEncoder());
+                            pipeline.addLast(new ChatServerHandler());
+                        }
+                    });
+            System.out.println("Srever 启动");
+            ChannelFuture sync = serverBootstrap.bind(6668).sync();
+            //对关闭通道进行监听
+            sync.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
