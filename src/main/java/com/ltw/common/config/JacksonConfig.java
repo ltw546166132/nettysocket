b195f0d18ef7ac3daa601f1761bfb6a76302a38b
“litianwei” <litianwei546166132@outlook.com>
2021-09-24 17:01:20 +0800

    update

--- src/main/java/com/ltw/common/config/JacksonConfig.java
+++ src/main/java/com/ltw/common/config/JacksonConfig.java
@@ -1,6 +1,14 @@
 package com.ltw.common.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 public class JacksonConfig {
